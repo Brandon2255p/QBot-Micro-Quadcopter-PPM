2aee2dd3f6fb0d3403920f44161fbe6b41e4f605
Brandon Piner <brandon2255p@gmail.com>
2015-12-27 19:58:46 +0200

    Changed to an interrupt based PPM

--- Arduino/.ino.cpp
+++ Arduino/.ino.cpp
@@ -1,23 +1,19 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2015-12-27 13:47:06
+//This file has been generated on 2015-12-27 19:10:38
 
 #include "Arduino.h"
 #include "Arduino.h"
 #include <stdio.h>
+#include "Arduino.h"
+#include "TimerOne.h"
+#include <stdint.h>
+#include <stdio.h>
 void setup() ;
 void loop()  ;
-void SetValues(int Thr, int Ail, int Ele, int Rud, int Ger) ;
-void PPMStateMachine() ;
-void SetSpace() ;
-void SetWait() ;
-void SetTHR() ;
-void SetAIL() ;
-void SetELE() ;
-void SetRUD() ;
-void SetGER() ;
-void SetVOID() ;
+void timerIsr() ;
+void StateMachine() ;
 
 #include "Quadcopter_PPM.ino"
 
--- Arduino/Quadcopter_PPM.ino
+++ Arduino/Quadcopter_PPM.ino
@@ -1,21 +1,21 @@
 #include "Arduino.h"
 #include <stdio.h>
 
-int PPM_Pin = 2;
-unsigned long tMark = 0;
-enum ePPM_State {WAIT, SPACE, THR, AIL, ELE, RUD, GER, VOID};
-unsigned long tPulseStart =0;
+#include "Arduino.h"
+#include "TimerOne.h"
+#include <stdint.h>
+#include <stdio.h>
+
+int32_t iPulseCount=0, iPeriodCount =0;
+
+#define PPMPin 2
+
+enum PPMState {prePulse0=0, P1, prePulse1, P2, prePulse2, P3, prePulse3, P4, prePulse4, P5, prePulse5};
+PPMState eCurrentState;
+
 char buffer[16];
 int counter =0;
 int iLoopCout = 0;
-
-struct PPM_State
-{
-  unsigned long wait;
-  ePPM_State eState;
-  ePPM_State eNextState;
-  int pinState;
-} sCurrentState;
 
 struct PPM_Values
 {
@@ -24,28 +24,34 @@ struct PPM_Values
 	int iEle;
 	int iRud;
 	int iGer;
-} sPPM_Values;
-
+};
+static struct PPM_Values sPPM_Values;
 void setup() {
-	pinMode(PPM_Pin, OUTPUT);  
-	pinMode(13, OUTPUT);
-	digitalWrite(13, LOW);
-	Serial.begin(115200);   
-	sCurrentState.eNextState = THR; 
-	sCurrentState.eState = SPACE;
-	sCurrentState.pinState = LOW;
-	sCurrentState.wait = 400;
-	tMark = micros();
-	tPulseStart = micros();
-	SetValues(755, 755, 755, 755, 755);
+	//Setup PPM
+	pinMode(PPMPin, OUTPUT);
+	eCurrentState = prePulse0;
+
+	Timer1.initialize(20); // set a timer of length 10 microseconds
+	Timer1.attachInterrupt( timerIsr ); // attach the service routine here
+
+	sPPM_Values.iThrust = 1500;
+	sPPM_Values.iAil 	= 750;
+	sPPM_Values.iEle 	= 750;
+	sPPM_Values.iRud 	= 750;
+	sPPM_Values.iGer	= 750;
+
+	//Setup Serial Comms
+	Serial.begin(115200);
+
 	Serial.print("hello there");
+
 }
 
 void loop() 
 {
 	iLoopCout++;
 	bool bEnoughData = false;
-	if(Serial.available() > 15) //wait for 16 bytes of data {0x0000,0x0000,0x0000,0x0000,0x0000}
+	if(Serial.available() > 15) //wait for 16 bytes of data {0x00 00,0x00 00,0x00 00,0x00 00,0x00 00}
 		bEnoughData = true;
 	while((bEnoughData)&&(Serial.available() > 0))
 	{
@@ -58,14 +64,13 @@ void loop()
 	char val1[3], val2[3],val3[3], val4[3],val5[3];
 	if (bEnoughData)
 	{
-		sscanf(buffer, "{%[^,],%[^,],%[^,],%[^,],%[^}]}", val1, val2, val3, val4, val5);
-		sPPM_Values.iThrust = (*(uint16_t*) &val1[0])&0xBFFF;
-		sPPM_Values.iAil 	= (*(uint16_t*) &val2[0])&0xBFFF;
-		sPPM_Values.iEle 	= (*(uint16_t*) &val3[0])&0xBFFF;
-		sPPM_Values.iRud 	= (*(uint16_t*) &val4[0])&0xBFFF;
-		sPPM_Values.iGer	= (*(uint16_t*) &val5[0])&0xBFFF;
+		sPPM_Values.iThrust = (*(uint16_t*) &buffer[1])&0xBFFF;
+		sPPM_Values.iAil 	= (*(uint16_t*) &buffer[4])&0xBFFF;
+		sPPM_Values.iEle 	= (*(uint16_t*) &buffer[7])&0xBFFF;
+		sPPM_Values.iRud 	= (*(uint16_t*) &buffer[10])&0xBFFF;
+		sPPM_Values.iGer	= (*(uint16_t*) &buffer[13])&0xBFFF;
 	}
-	/*if(iLoopCout%10000==0)
+/*	if(iLoopCout%10==0)
 	{
 		Serial.print(sPPM_Values.iThrust);
 		Serial.print(" ");
@@ -79,133 +84,127 @@ void loop()
 		Serial.println(" ");
 	}*/
 	counter=0;
+	StateMachine();
+}
+/// --------------------------
+/// Custom ISR Timer Routine
+/// --------------------------
+void timerIsr()
+{
+	iPeriodCount++;
+	iPulseCount++;
 
 
-	SetValues(sPPM_Values.iThrust, sPPM_Values.iAil, sPPM_Values.iEle, sPPM_Values.iRud, sPPM_Values.iGer);
-	PPMStateMachine();
 }
 
-void SetValues(int Thr, int Ail, int Ele, int Rud, int Ger)
+void StateMachine()
 {
-	sPPM_Values.iThrust = Thr;
-	sPPM_Values.iAil = Ail;
-	sPPM_Values.iEle= Ele;
-	sPPM_Values.iRud= Rud;
-	sPPM_Values.iGer= Ger;
-}
-
-void PPMStateMachine()
-{
-	unsigned long timeDiff = micros() - tMark;
-		unsigned tPulseLength = micros() - tPulseStart;
-		if(tPulseLength < 20000)
+	if(iPeriodCount>=900)
 		{
-			if(timeDiff >= sCurrentState.wait)
+			iPeriodCount =0;
+			iPulseCount =0;
+			eCurrentState = prePulse0;//begin the pulse
+			digitalWrite(PPMPin, 0);//Set low to start pulse
+		}
+
+	switch(eCurrentState)
+	{
+		case prePulse0:
+			if(iPulseCount >= 20)
 			{
-				switch (sCurrentState.eState)
-				{
-				case WAIT:
-					//SetSpace();
-				break;
-				case SPACE:
-					/// Switch for space
-					switch(sCurrentState.eNextState)
-					{
-					case THR:
-						SetTHR();
-						break;
-					case AIL:
-						SetAIL();
-						break;
-					case ELE:
-						SetELE();
-						break;
-					case RUD:
-						SetRUD();
-						break;
-					case GER:
-						SetGER();
-						break;
-					case VOID:
-						SetVOID();
-						break;
-					case WAIT:
-						SetWait();
-						break;
-					default:
-						sCurrentState.eState = WAIT;
-						sCurrentState.eNextState = SPACE;
-					} /// End switch for space
-				  break;
-				default:
-				  SetSpace();
-				}
-				tMark = micros();
-				digitalWrite(PPM_Pin, sCurrentState.pinState);
+				iPulseCount =0;
+				eCurrentState = P1;//After waiting 400 us go to next pulse
+				digitalWrite(PPMPin, 1);
 			}
-		}
-		else
-		{
-			tPulseStart = micros();
-			SetSpace();
-		}
-}
+		break;
 
-void SetSpace()
-{
-	sCurrentState.eState = SPACE;
-	sCurrentState.pinState = LOW;
-	sCurrentState.wait = 390;
-}
+		case P1:
+			if(iPulseCount >= sPPM_Values.iThrust/20)
+			{
+				iPulseCount =0;
+				eCurrentState = prePulse1;//After waiting 400 us go to next pulse
+				digitalWrite(PPMPin, 0);
+			}
+		break;
 
-void SetWait()
-{
-	sCurrentState.eState = WAIT;
-	sCurrentState.eNextState = VOID;
-	sCurrentState.pinState = HIGH;
-	sCurrentState.wait = 7000;
-}
+		case prePulse1:
+			if(iPulseCount >= 20)
+			{
+				iPulseCount =0;
+				eCurrentState = P2;//After waiting 400 us go to next pulse
+				digitalWrite(PPMPin, 1);
+			}
+		break;
 
-void SetTHR()
-{
-	sCurrentState.eState = THR;
-	sCurrentState.eNextState = AIL;
-	sCurrentState.pinState = HIGH;
-	sCurrentState.wait = sPPM_Values.iThrust;
-}
-void SetAIL()
-{
-	sCurrentState.eState = AIL;
-	sCurrentState.eNextState = ELE;
-	sCurrentState.pinState = HIGH;
-	sCurrentState.wait = sPPM_Values.iAil;
-}
-void SetELE()
-{
-	sCurrentState.eState = ELE;
-	sCurrentState.eNextState = RUD;
-	sCurrentState.pinState = HIGH;
-	sCurrentState.wait = sPPM_Values.iEle;
-}
-void SetRUD()
-{
-	sCurrentState.eState = RUD;
-	sCurrentState.eNextState = GER;
-	sCurrentState.pinState = HIGH;
-	sCurrentState.wait = sPPM_Values.iRud;
-}
-void SetGER()
-{
-	sCurrentState.eState = GER;
-	sCurrentState.eNextState = WAIT;
-	sCurrentState.pinState = HIGH;
-	sCurrentState.wait = sPPM_Values.iGer;
-}
+		case P2:
+			if(iPulseCount >= sPPM_Values.iAil/20)
+			{
+				iPulseCount =0;
+				eCurrentState = prePulse2;//After waiting 400 us go to next pulse
+				digitalWrite(PPMPin, 0);
+			}
+		break;
 
-void SetVOID()
-{
-	sCurrentState.eState = VOID;
-	sCurrentState.eNextState = THR;
-	sCurrentState.pinState = HIGH;
-	sCurrentState.wait = 500;// Time doesnt do anything
+		case prePulse2:
+			if(iPulseCount >= 20)
+			{
+				iPulseCount =0;
+				eCurrentState = P3;//After waiting 400 us go to next pulse
+				digitalWrite(PPMPin, 1);
+			}
+		break;
+
+		case P3:
+			if(iPulseCount >= sPPM_Values.iEle/20)
+			{
+				iPulseCount =0;
+				eCurrentState = prePulse3;//After waiting 400 us go to next pulse
+				digitalWrite(PPMPin, 0);
+			}
+		break;
+
+		case prePulse3:
+			if(iPulseCount >= 20)
+			{
+				iPulseCount =0;
+				eCurrentState = P4;//After waiting 400 us go to next pulse
+				digitalWrite(PPMPin, 1);
+			}
+		break;
+
+		case P4:
+			if(iPulseCount >= sPPM_Values.iRud/20)
+			{
+				iPulseCount =0;
+				eCurrentState = prePulse4;//After waiting 400 us go to next pulse
+				digitalWrite(PPMPin, 0);
+			}
+		break;
+
+		case prePulse4:
+			if(iPulseCount >= 20)
+			{
+				iPulseCount =0;
+				eCurrentState = P5;//After waiting 400 us go to next pulse
+				digitalWrite(PPMPin, 1);
+			}
+		break;
+
+		case P5:
+			if(iPulseCount >= sPPM_Values.iGer/20)
+			{
+				iPulseCount =0;
+				eCurrentState = prePulse5;//After waiting 400 us go to next pulse
+				digitalWrite(PPMPin, 0);
+			}
+		break;
+
+		case prePulse5:
+			if(iPulseCount >= 20)
+			{
+				digitalWrite(PPMPin, 1);
+			}
+		break;
+
+	}
 }
