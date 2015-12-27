7075aaf1eab2c434edb079ed5a05a05f081c588e
Brandon Piner <brandon2255p@gmail.com>
2015-12-27 13:48:36 +0200

    Added arduino code

---
+++ Arduino/.ino.cpp
@@ -0,0 +1,23 @@
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2015-12-27 13:47:06
+
+#include "Arduino.h"
+#include "Arduino.h"
+#include <stdio.h>
+void setup() ;
+void loop()  ;
+void SetValues(int Thr, int Ail, int Ele, int Rud, int Ger) ;
+void PPMStateMachine() ;
+void SetSpace() ;
+void SetWait() ;
+void SetTHR() ;
+void SetAIL() ;
+void SetELE() ;
+void SetRUD() ;
+void SetGER() ;
+void SetVOID() ;
+
+#include "Quadcopter_PPM.ino"
+
---
+++ Arduino/spec.d
@@ -0,0 +1,2 @@
+spec.o: \
+ /home/brandon/Coded\ Programs/Arduino/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
